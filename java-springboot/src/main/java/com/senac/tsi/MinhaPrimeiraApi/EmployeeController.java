73ef29d3733979d7a9469d025cfd74b66158ec05
Beynd-Isaac <isaacvcarv4lho@gmail.com>
2026-08-28 10:28:06 -0300

    feat: implement full CRUD controller for Employee entity

--- java-springboot/src/main/java/com/senac/tsi/MinhaPrimeiraApi/EmployeeController.java
+++ java-springboot/src/main/java/com/senac/tsi/MinhaPrimeiraApi/EmployeeController.java
@@ -1,10 +1,10 @@
 package com.senac.tsi.MinhaPrimeiraApi;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class EmployeeController {
@@ -19,4 +19,32 @@ public class EmployeeController {
     public List<Employee> GetAll(){
         return repository.findAll();
     }
+
+    @PostMapping("/employee")
+    public Employee createEmployee(@RequestBody Employee newEmployee){
+        return repository.save(newEmployee);
+
+    }
+
+    @GetMapping("/employee/{id}")
+    public Optional <Employee> getEmployeeById(@PathVariable long id){
+        return repository.findById(id);
+    }
+
+    @PutMapping("/employee/id")
+    public Employee updateOrCreateEmployee(@RequestBody Employee newEmployee, @PathVariable long id){
+        return repository.findById(id)
+                .map(employee -> {
+                    employee.setName(newEmployee.getName());
+                    employee.setRole(newEmployee.getRole());
+                    return repository.save(employee);
+                }).orElseGet(() ->
+                        repository.save(newEmployee));
+    }
+
+    @DeleteMapping("/employee/{id}")
+    public void deleteEmployeeById (@PathVariable long id){
+        repository.deleteById(id);
+    }
+
 }
